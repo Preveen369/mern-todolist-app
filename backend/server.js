ec400d7b8a21396eee60c656c85ffe5fdc5a2432
Preveen369 <spreveen123@gmail.com>
2025-05-22 07:01:46 +0530

    used env variable for API URL in frontend

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,13 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const todoRoutes = require('./routes/todoRoutes');
+
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ origin: 'https://mern-todolist-app-backend-a5k8.onrender.com', credentials: true }));
+app.use(cors({ origin: 'https://mern-todolist-app-2gws.onrender.com', credentials: true }));
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI, {
@@ -21,7 +22,7 @@ app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
