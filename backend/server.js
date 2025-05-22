fdad744641c58ffbe5620ba5f2f1f4849f2222b3
Preveen369 <spreveen123@gmail.com>
2025-05-22 06:14:37 +0530

    Update CORS configuration

--- backend/server.js
+++ backend/server.js
@@ -9,10 +9,9 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({ origin: 'https://mern-todolist-app-2gws.onrender.com', credentials: true }));
 app.use(express.json());
 
-// Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
